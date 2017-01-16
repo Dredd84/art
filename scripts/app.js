859d17f1e8799be0f4250d45817b4f22a94c8897
Dredd84 <dto1984@gmail.com>
2017-01-16 18:10:00 -0500

    Update Nav Links

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-$('.dropdown-toggle').dropdown()
+setNavWidth: function(){
+  var wi = parseInt(($('navbar-nav').width()-48)*.25);
+  for( var i = 0 ; i < 4; i++) {
+    $('navli' +i).width(wi);
+  }
+}
